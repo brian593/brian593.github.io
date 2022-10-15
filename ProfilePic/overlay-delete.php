9e69471c5331951b84b6efc6d0b02f1d3aab7016
brianmolinabastidas <brian593@live.com>
2022-10-15 17:40:41 -0500

    delet

--- ProfilePic/overlay-delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DIR_LOC', 'images/');
-$img = $_POST['img'];
-$img = filter_var($img, FILTER_SANITIZE_STRING);
-$file_path = DIR_LOC . $img . '.png';
-$successful_delete = unlink($file_path);
-print $successful_delete ? 'Successfully deleted.' : 'Deletion unsuccessful.';
--- overlay/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset( $_SESSION['access_token'] );
-unset( $_SESSION['fb_state'] );
-
-
-  header('Location: http://www.virtualrival.mainsusl.com');
-
- ?>
